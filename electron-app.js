d247e01e460bb18e76c7a7a07d85e950943bbe61
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-11-12 00:19:51 +0530

    5. add IPC

--- electron-app.js
+++ electron-app.js
@@ -1,13 +1,32 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, ipcMain } from 'electron';
 import path, { dirname } from 'node:path';
 import { fileURLToPath, format } from 'node:url';
+import ffmpeg from 'fluent-ffmpeg';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+let mainWindow;
+
 app.on('ready', () => {
-    const mainWindow = new BrowserWindow({})
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    })
     mainWindow.loadURL(format({
         pathname: path.resolve(__dirname, './frontend/index.html'),
     }))
     mainWindow.webContents.openDevTools()
+})
+
+ipcMain.on('video:submit', (event, data) => {
+    console.log("🚀 ~ event:", event)
+    console.log("🚀 ~ data:", data)
+    // ffmpeg.ffprobe(data, (err, metadata) => {
+    // console.log("🚀 ~ metadata:", metadata)
+    // console.log("🚀 ~ err:", err)
+    const length = Math.random() * 100
+    mainWindow.webContents.send('video:length', { length })
+    // })
 })
