b8d34203dd11c4ea104f61a7b7d44496db403d48
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-11-11 23:14:03 +0530

    3. load basic html

--- electron-app.js
+++ electron-app.js
@@ -1,5 +1,13 @@
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow } from 'electron';
+import path, { dirname } from 'node:path';
+import { fileURLToPath, format } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.on('ready', () => {
     const mainWindow = new BrowserWindow({})
+    mainWindow.loadURL(format({
+        pathname: path.resolve(__dirname, './frontend/index.html'),
+    }))
+    mainWindow.webContents.openDevTools()
 })
