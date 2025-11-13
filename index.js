5f18ab947bf91696b1e91baee8a5666fd7fdcc59
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-11-13 23:35:02 +0530

    inital setup commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import { app, BrowserWindow } from 'electron';
+
+// Keep a global reference of mainwindow so it is not garbage collected.
+let mainWindow;
+
+// Create window on app ready
+function createWindow() {
+  // create and set reference
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 800,
+  });
+
+  mainWindow.loadFile('index.html');
+
+  // Open dev tools for debugging.
+  mainWindow.webContents.openDevTools();
+
+  // unset the reference on close
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+// Listen the app ready event
+app.on('ready', () => {
+  console.log('App is ready');
+  createWindow();
+});
