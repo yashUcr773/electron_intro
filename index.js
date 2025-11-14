8ef62297f6c21b23ea423af75f68dd9041bf0aa3
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-11-14 22:04:04 +0530

    webframe

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1000,
     height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
   });
 
   mainWindow.loadFile('index.html');
