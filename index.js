918642b8b2d51cefa0f6683a693f6c2433f9b591
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-11-14 14:30:29 +0530

    shortcuts

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, globalShortcut } from 'electron';
 
 // Keep a global reference of mainwindow so it is not garbage collected.
 let mainWindow;
@@ -15,6 +15,18 @@ function createWindow() {
 
   // Open dev tools for debugging.
   mainWindow.webContents.openDevTools();
+
+  // we can add listeners on webcontents as well.
+  // what this provides is a listener even if app is not in focus and then keys are pressed.
+  globalShortcut.register('g', () => {
+    console.log('pressed g');
+  });
+
+  globalShortcut.register('CommandorControl+g', () => {
+    console.log('ctrl g');
+  });
+
+  globalShortcut.unregister('h');
 
   // unset the reference on close
   mainWindow.on('closed', () => {
