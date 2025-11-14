d7aa6b7a35d7207383d955b660b2b6e653acb51a
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-11-14 13:42:04 +0530

    2. cookies

--- index.js
+++ index.js
@@ -1,66 +1,52 @@
 import { app, BrowserWindow, session } from 'electron';
 
 // Keep a global reference of mainwindow so it is not garbage collected.
-let mainWindow, secWindow, tertWindow;
+let mainWindow;
 
 // Create window on app ready
-function createWindow() {
-  // Create custom sessions
-  // in memory
-  const customSes = session.fromPartition('part1');
-  // persisted
-  const customPersistantSes = session.fromPartition('persist:part2');
+async function createWindow() {
+  // get cookies
+  // const cookies = await session.defaultSession.cookies.get({});
+  // console.log('🚀 ~ createWindow ~ cookies:', cookies);
+
+  // filter cookies
+  // const cookies = await session.defaultSession.cookies.get({ name: 'cookie1' });
+  // console.log('🚀 ~ createWindow ~ cookies:', cookies);
+
+  // set cookies
+  await session.defaultSession.cookies.set({
+    url: 'https://mydomain.com',
+    name: 'cookie1',
+    value: 'myval',
+    expirationDate: new Date().getTime() + 10000,
+  });
+
+  // remove cookies
+  // await session.defaultSession.cookies.remove('https://mydomain.com', 'cookie1');
 
   // create and set reference
   mainWindow = new BrowserWindow({
     width: 1000,
     height: 800,
-  });
-
-  secWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
     webPreferences: {
-      session: customSes,
+      partition: 'abc',
     },
   });
 
-  tertWindow = new BrowserWindow({
-    width: 600,
-    height: 400,
-    webPreferences: {
-      session: customPersistantSes,
-      // partition: 'persist:name'
-    },
+  // mainWindow.loadFile('index.html');
+  mainWindow.loadURL('https://yashaggarwal.com');
+
+  // Open dev tools for debugging.
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.webContents.on('did-finish-load', async () => {
+    const cookies = await session.defaultSession.cookies.get({});
+    console.log('🚀 ~ createWindow ~ cookies:', cookies);
   });
-
-  mainWindow.loadFile('index.html');
-  secWindow.loadFile('index.html');
-  tertWindow.loadFile('index.html');
-
-  // get default session
-  const ses = mainWindow.webContents.session;
-  console.log(ses);
-  const ses2 = secWindow.webContents.session;
-  console.log(ses2);
-  const defaultSes = session.defaultSession;
-  console.log(Object.is(ses, ses2));
-  console.log(Object.is(ses, defaultSes));
-  console.log(Object.is(ses, customSes));
 
   // unset the reference on close
   mainWindow.on('closed', () => {
     mainWindow = null;
-  });
-
-  // unset the reference on close
-  secWindow.on('closed', () => {
-    secWindow = null;
-  });
-
-  // unset the reference on close
-  tertWindow.on('closed', () => {
-    tertWindow = null;
   });
 }
 
