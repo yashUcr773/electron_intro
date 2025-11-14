0ac57878711a67088364b40f13f22647fe9628e7
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-11-14 16:04:45 +0530

    power monitor events

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron';
+import electron, { app, BrowserWindow } from 'electron';
 
 // Keep a global reference of mainwindow so it is not garbage collected.
 let mainWindow;
@@ -15,6 +15,30 @@ function createWindow() {
 
   // Open dev tools for debugging.
   mainWindow.webContents.openDevTools();
+
+  electron.powerMonitor.on('on-ac', () => {
+    console.log('On AC');
+  });
+
+  electron.powerMonitor.on('on-battery', () => {
+    console.log('On battery');
+  });
+
+  electron.powerMonitor.on('lock-screen', () => {
+    console.log('lock-screen');
+  });
+
+  electron.powerMonitor.on('resume', () => {
+    console.log('resume');
+  });
+
+  electron.powerMonitor.on('suspend', () => {
+    console.log('suspend');
+  });
+
+  electron.powerMonitor.on('unlock-screen', () => {
+    console.log('unlock-screen');
+  });
 
   // unset the reference on close
   mainWindow.on('closed', () => {
