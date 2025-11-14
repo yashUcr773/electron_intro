bc3c28b4a3229a035c959e10d94610fcc725e02f
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-11-14 14:54:05 +0530

    1. menubar menus and submenus

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, Menu, MenuItem } from 'electron';
 
 // Keep a global reference of mainwindow so it is not garbage collected.
 let mainWindow;
+
+// let mainMenu = new Menu();
+// let menuItem1 = new MenuItem({
+//   label: 'Menu 1',
+//   submenu: [
+//     { label: 'Item1' },
+//     {
+//       label: 'Item2',
+//       submenu: [
+//         { label: 'submenu1' },
+//         { label: 'submenu2', click: () => console.log('click submenu2') },
+//         { label: 'submenu3' },
+//       ],
+//     },
+//     { label: 'Item3', click: () => console.log('click item 3') },
+//   ],
+// });
+
+// mainMenu.append(menuItem1);
+
+const mainMenu = Menu.buildFromTemplate([
+  {
+    label: 'Menu 1',
+    submenu: [
+      { label: 'Item1' },
+      {
+        label: 'Item2',
+        submenu: [
+          { label: 'submenu1' },
+          { label: 'submenu2', click: () => console.log('click submenu2') },
+          { label: 'submenu3' },
+        ],
+      },
+      { label: 'Item3', click: () => console.log('click item 3'), accelerator: 'Shift+alt+g' },
+    ],
+  },
+  {
+    label: 'Menu 2',
+    submenu: [
+      { label: 'Item1' },
+      {
+        label: 'Item2',
+        enabled: false,
+      },
+      { label: 'Item3' },
+      { label: 'dev tools', role: 'toggleDevTools' },
+      { role: 'togglefullscreen' },
+    ],
+  },
+]);
 
 // Create window on app ready
 function createWindow() {
@@ -15,6 +65,8 @@ function createWindow() {
 
   // Open dev tools for debugging.
   mainWindow.webContents.openDevTools();
+
+  Menu.setApplicationMenu(mainMenu);
 
   // unset the reference on close
   mainWindow.on('closed', () => {
