86e3be772d3603ebdcf20fce680aaf53a033a6c6
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-11-15 23:06:21 +0530

    web preload scripts

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ import { app, BrowserWindow } from 'electron';
 // Keep a global reference of mainwindow so it is not garbage collected.
 let mainWindow;
 
+console.log(`${process.cwd()}/preload.js`);
 // Create window on app ready
 function createWindow() {
   // create and set reference
   mainWindow = new BrowserWindow({
     width: 1000,
     height: 800,
+    webPreferences: {
+      // we need to disable node integration and context isolation for security
+      // nodeIntegration: true,
+      // contextIsolation: false,
+      preload: `${process.cwd()}/preload.js`,
+    },
   });
 
   mainWindow.loadFile('index.html');
---
+++ preload.js
@@ -0,0 +1,2 @@
+const { contextBridge } = require('electron');
+contextBridge.exposeInMainWorld('versions', process.versions);
